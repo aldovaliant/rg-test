3d46e53ea5ec85f7795b6ec632f7c95fd1d35df7
Aldo Verrell Valiant <aldo@algrow.co>
2021-12-07 05:41:01 +0700

    react-create-app on Heroku

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
-const pages = ['Dashboard', 'Form'];
+// const pages = ['Dashboard', 'Form'];
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
